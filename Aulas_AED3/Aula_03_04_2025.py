26eabddffaa17689ab1bf2fcfdd71ee41363d5fe
Gabriel Cassino <120288615+kasshinokun@users.noreply.github.com>
2025-04-04 11:36:58 +0900

    Update Aula_03_04_2025.py

--- Aulas_AED3/Aula_03_04_2025.py
+++ Aulas_AED3/Aula_03_04_2025.py
@@ -1,13 +1,24 @@
 import streamlit as st
+'''
+vetor=[0]*13
 
+grid =st.columns(13)
+with grid[0]:
+    st.text_input("pos ", value=vetor [0], max_chars=1,disabled=False)
+with grid[1]:
+    st.text_input("pos ", value=vetor [1], max_chars=1,disabled=False)
+
+
+'''
 def visualArray(value):
-
 # Function to create a row of widgets (with row number input to assure unique keys)
-
-  # columns to lay out the inputs
+   vetor = ["empty"]*value;
+   # columns to lay out the inputs
    grid = st.columns(value)
    # Loop to create rows of input widgets
-   for i in range(num_rows):
+   for i in range(value):
         with grid[i]:
-            st.text_input('col{i+1}', key=f'input_col1{row}')
-    
+            st.text_input("pos {int(i)}", value=vetor [i], max_chars=1,disabled=False)
+st.title("Aula de 03-04-2025")
+visualArray(13)
+
